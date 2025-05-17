6e7c7b58b0923328a80ed551e033d0daccee2dd0
Khandelwal6350 <2023pietcakrish033@poornima.org>
2025-05-17 08:48:14 +0530

    new

---
+++ leet code/may/SolutionNo838.java
@@ -0,0 +1,46 @@
+public class SolutionNo838 {
+    public String pushDominoes(String dominoes) {
+        int n = dominoes.length();
+        int[] forces = new int[n];
+        int force = 0;
+
+        // Left to Right Pass
+        for (int i = 0; i < n; i++) {
+            if (dominoes.charAt(i) == 'R') {
+                force = n;
+            } else if (dominoes.charAt(i) == 'L') {
+                force = 0;
+            } else {
+                force = Math.max(force - 1, 0);
+            }
+            forces[i] += force;
+        }
+
+        // Right to Left Pass
+        force = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (dominoes.charAt(i) == 'L') {
+                force = n;
+            } else if (dominoes.charAt(i) == 'R') {
+                force = 0;
+            } else {
+                force = Math.max(force - 1, 0);
+            }
+            forces[i] -= force;
+        }
+
+        // Determine final state
+        StringBuilder result = new StringBuilder();
+        for (int f : forces) {
+            if (f > 0) {
+                result.append('R');
+            } else if (f < 0) {
+                result.append('L');
+            } else {
+                result.append('.');
+            }
+        }
+
+        return result.toString();
+    }
+}
