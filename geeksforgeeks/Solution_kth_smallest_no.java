5a79052c8a4c4b2364e013276a2afb76c6f346b6
Khandelwal6350 <2023pietcakrish033@poornima.org>
2025-05-21 19:39:57 +0530

    new

---
+++ geeksforgeeks/Solution_kth_smallest_no.java
@@ -0,0 +1,28 @@
+package geeksforgeeks;
+
+public class Solution_kth_smallest_no {
+   
+        public int kthSmallest(int m, int n, int k) {
+            int low = 1, high = m * n;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                int count = countLessEqual(mid, m, n);
+                if (count < k) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+            return low;
+        }
+    
+        private int countLessEqual(int val, int m, int n) {
+            int count = 0;
+            for (int i = 1; i <= m; i++) {
+                count += Math.min(val / i, n);
+            }
+            return count;
+        }
+    }
+    
+
--- leet code/april/SolutionNo1920.java
+++ leet code/april/SolutionNo1920.java
@@ -1,4 +1,4 @@
-class Solution {
+class SolutionNo1920 {
     public int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
